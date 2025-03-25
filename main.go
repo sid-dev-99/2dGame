881133550da94ec9cb985077fa28fe09040151ac
sid-dev-99 <engrsiddharth1@gmail.com>
2025-03-25 19:28:21 +0530

    adding gameLogic

--- main.go
+++ main.go
@@ -1,25 +1,71 @@
 package main 
 import (
 	"fmt"
-	"os"
-	"os/exec"
-	"runtime"
+	"log"
 	"github.com/gdamore/tcell"
 )
 
 func main(){
-	// fmt.Println("hello world");
 
-	screen,err = tcell.Newscreen()
+	screen,err := tcell.NewScreen()
 	if err != nil{
 		log.Fatalf("%+v",err)
 	}
 
-	if err = screen.Init(); err != nil {
-		log.fatalF("%+v",err)
+	defer screen.Fini()
+
+	if err := screen.Init(); err != nil {
+		log.Fatalf("%+v",err)
+	}
+
+	//global var
+
+	
+
+	// running loop 
+
+	isRunning := true
+
+	for isRunning{
+
+
+	screen.Clear()
+
+	//draw something here
+
+	screen.SetContent(10,10,'@',nil,tcell.StyleDefault)
+
+	screen.Show()
+
+
+
+		ev := screen.PollEvent()
+		switch ev := ev.(type){
+		case *tcell.EventKey:
+			switch ev.Rune(){
+			case 'q':
+				isRunning = false
+			case 'a':
+			}
+		}
+
 	}
 
 	
+
+
+	w,h := screen.Size()
+	fmt.Println("size is %d\n%d", w ,h)
+	//screen.Clear()
+
+
+	// draw
+
+	//screen.setContext()
+
+	//
+	//screen.show()
+
 
 	fmt.Print("done")
 }
---
+++ sprite.go
@@ -0,0 +1,14 @@
+package main
+
+type Sprite interface(){
+	X int
+	Y int
+	C rune 
+	
+}
+
+
+
+func main(){
+
+}
